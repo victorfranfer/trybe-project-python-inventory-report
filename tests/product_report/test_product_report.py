6470084cc1a1f099d702574501269e6fc15bc3db
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-03 18:49:59 -0300

    req8:test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id="1",
+        nome_do_produto="Nicotine Polacrilex",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2021-02-18",
+        data_de_validade="2023-09-17",
+        numero_de_serie="CR25 1551 4467 2549 4402 1",
+        instrucoes_de_armazenamento="instrucao 1",
+        )
+
+    assert str(product) == (
+        "O produto Nicotine Polacrilex"
+        " fabricado em 2021-02-18"
+        " por Target Corporation com validade"
+        " até 2023-09-17"
+        " precisa ser armazenado instrucao 1."
+    )
