0a3d4bb0ffcf458d86259dda6a7b5c0eb06089b9
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-03 16:50:34 -0300

    req3:complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,34 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, stock):
+        simple_report = super().generate(stock)
+        products_by_company = cls.get_products_stocked_by_company(stock)
+        companies_report = cls.generate_companies_report(products_by_company)
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies_report}"
+        )
+
+    @staticmethod
+    def get_products_stocked_by_company(stock):
+        companies = {}
+        for product in stock:
+            company = product["nome_da_empresa"]
+            if company in companies:
+                companies[company] += 1
+            else:
+                companies[company] = 1
+        return companies
+
+    @staticmethod
+    def generate_companies_report(companies):
+        report = "".join(
+            f"- {company}: {quantity}\n" for company,
+            quantity in companies.items()
+        )
+        return report
