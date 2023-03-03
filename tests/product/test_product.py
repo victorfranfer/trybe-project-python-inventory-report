2665f2f66b696ec0ef78da998ad0074e1b9d9935
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-03 15:51:49 -0300

    req1:test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
-#initial commit
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id="1",
+        nome_do_produto="Nicotine Polacrilex",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2021-02-18",
+        data_de_validade="2023-09-17",
+        numero_de_serie="CR25 1551 4467 2549 4402 1",
+        instrucoes_de_armazenamento="instrucao 1",
+        )
+
+    assert product.id == "1"
+    assert product.nome_do_produto == "Nicotine Polacrilex"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == "2021-02-18"
+    assert product.data_de_validade == "2023-09-17"
+    assert product.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert product.instrucoes_de_armazenamento == "instrucao 1"
