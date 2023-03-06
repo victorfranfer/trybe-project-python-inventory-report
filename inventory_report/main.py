841f6ee448063d92b3fb0e6bf923c8d34a07fd16
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-06 14:09:45 -0300

    req11:main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,29 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from os import path
+
+
+def __choose_importer(file_extension):
+    if file_extension == ".csv":
+        return CsvImporter
+    if file_extension == ".json":
+        return JsonImporter
+    if file_extension == ".xml":
+        return XmlImporter
+
+
 def main():
-    pass
+    arguments = sys.argv
+    if len(arguments) != 3:
+        sys.stderr.write("Verifique os argumentos\n")
+        return sys.stderr
+    file = arguments[1]
+    type = arguments[2]
+    file_extension = path.splitext(file)[1]
+    importer = __choose_importer(file_extension)
+    inventory = InventoryRefactor(importer)
+    data = inventory.import_data(file, type)
+    print(data, end="")
